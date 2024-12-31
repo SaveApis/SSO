a2b20803afcb0279795d35fd78c55869f1d6be35
SaveApis | Felix Hake <felix.hake@saveapis.com>
2024-12-31 15:11:56 +0100

    FIX(user): Fix phone number validation
    Author: Felix Hake

--- src/Backend/Domains/User/Domain/VO/Phone.cs
+++ src/Backend/Domains/User/Domain/VO/Phone.cs
@@ -1,25 +1,17 @@
-﻿using System.Text.RegularExpressions;
-using Vogen;
+﻿using Vogen;
 
 namespace Backend.Domains.User.Domain.VO;
 
 [ValueObject<string>(Conversions.EfCoreValueConverter)]
 public partial class Phone
 {
-    private const string RegexPattern =
-        @"\+(9[976]\d|8[987530]\d|6[987]\d|5[90]\d|42\d|3[875]\d|2[98654321]\d|9[8543210]|8[6421]|6[6543210]|5[87654321]|4[987654310]|3[9643210]|2[70]|7|1)\W*\d\W*\d\W*\d\W*\d\W*\d\W*\d\W*\d\W*\d\W*(\d{1,2})$";
-
     private static string NormalizeInput(string input)
     {
         return input;
     }
 
-    private static Validation Validate(string input)
+    private static Validation Validate(string _)
     {
-        var regex = new Regex(RegexPattern);
-
-        return regex.Match(input).Success
-            ? Validation.Ok
-            : Validation.Invalid("Invalid phone number!");
+        return Validation.Ok;
     }
 }
